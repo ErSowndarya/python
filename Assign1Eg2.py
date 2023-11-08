4a41ff7d3ebcb9c23a8734fa2bd74349f8be9dd9
ErSowndarya <sowndaryadcareer@gmail.com>
2023-11-08 22:23:46 +0530

    Removed files

--- Assign1Eg2.py
+++
@@ -1,9 +0,0 @@
-marks=int(input("Enter marks in the exam:"))
-if(marks<=50):
-    print("A grade")
-elif(marks<=75 and marks>=51):
-    print("B grade")
-elif(marks<=121 and marks<=150):
-    print("S grade")
-else:
-    print("Invalid")
