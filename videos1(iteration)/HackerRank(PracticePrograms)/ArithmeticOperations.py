1213b4d5e883034d46207a6ffae6ad30431a3312
ErSowndarya <sowndaryadcareer@gmail.com>
2024-04-16 22:45:11 +0530

    video 8 to 10

---
+++ videos1(iteration)/HackerRank(PracticePrograms)/ArithmeticOperations.py
@@ -0,0 +1,14 @@
+#The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+#The first line contains the sum of the two numbers.
+#The second line contains the difference of the two numbers (first - second).
+#The third line contains the product of the two numbers.
+
+a = int(input())
+b = int(input())
+Addition=a+b
+Subtraction=a-b
+Multiplication=a*b
+print(Addition)
+print(Subtraction)
+print(Multiplication)
+ 
